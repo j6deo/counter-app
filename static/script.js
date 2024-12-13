a3b03338bf2384f61f5cf8da2207f94458832121
j6deo <jaydenpoints@icloud.com>
2024-12-13 16:49:27 -0500

    Add files via upload

---
+++ static/script.js
@@ -0,0 +1,8 @@
+let btn =document.getElementById("btn")
+btn.addEventListener('click', function(){
+    let counter = document.getElementById("counter")
+    counter.innerHTML = pareseInt(counter.innerHTML)+1
+
+
+
+})
